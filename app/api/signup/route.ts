697eceb41f8da194286e3d515b70b5debbe97ecf
Annie Huang <annie.hn@gmail.com>
2024-08-31 16:20:13 +1000

    safeParse the return body return from the server

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,5 +1,11 @@
 import { NextResponse } from 'next/server';
+import { signUpSchema } from '@/lib/types';
 
 export async function POST(request: Request): Promise<void> {
+  const body: unknown = await request.json();
+
+  // Compared to .parse, .safeParse will not throw error when it entercounter error.
+  const result = signUpSchema.safeParse(body);
+
   return NextResponse.json({});
 }
