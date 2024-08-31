f3ba2dcb41148cd870ebcf04a8fadb97aa5473ff
Annie Huang <annie.hn@gmail.com>
2024-08-31 14:15:07 +1000

    We don't need to use value and onChange for each input and removed the onSubmit and errors display as well

--- components/form-with-react-hook-form.tsx
+++ components/form-with-react-hook-form.tsx
@@ -13,38 +13,20 @@ export const FormWithReactHookForm = () => {
   } = useForm();
 
   return (
-    <form onSubmit={handleSubmit} className='flex flex-col gap-y-2'>
-      {errors.length > 0 && (
-        <ul>
-          {errors.map((error) => (
-            <li
-              key={error}
-              className='bg-red-100 text-red-500 px-4 py-2 rounded'
-            >
-              {error}
-            </li>
-          ))}
-        </ul>
-      )}
+    <form className='flex flex-col gap-y-2'>
       <input
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
         type='email'
         required
         placeholder='Email'
         className='px-4 py-2 rounded'
       />
       <input
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
         type='password'
         required
         placeholder='Password'
         className='px-4 py-2 rounded'
       />
       <input
-        value={confirmPassword}
-        onChange={(e) => setConfirmPassword(e.target.value)}
         type='password'
         required
         placeholder='Confirm password'
@@ -53,7 +35,6 @@ export const FormWithReactHookForm = () => {
 
       <button
         type='submit'
-        disabled={isSubmitting}
         className='bg-blue-500 disabled:bg-gray-500 py-2 rounded'
       >
         Submit
