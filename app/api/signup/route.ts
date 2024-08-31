e6057b60f20bf571260e9e1a3126335516b4e1e5
Annie Huang <annie.hn@gmail.com>
2024-08-31 16:35:49 +1000

    Update route.ts to return zod errors if there is anything failed validation.

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,11 +1,23 @@
 import { NextResponse } from 'next/server';
 import { signUpSchema } from '@/lib/types';
 
-export async function POST(request: Request): Promise<void> {
+// export async function POST(request: Request): Promise<void> {
+export async function POST(request: Request) {
   const body: unknown = await request.json();
 
   // Compared to .parse, .safeParse will not throw error when it entercounter error.
   const result = signUpSchema.safeParse(body);
 
-  return NextResponse.json({});
+  let zodErrors = {};
+  if (!result.success) {
+    result.error.issues.forEach((issue) => {
+      zodErrors = { ...zodErrors, [issue.path[0]]: issue.message };
+    });
+  }
+
+  return NextResponse.json(
+    Object.keys(zodErrors).length > 0
+      ? { errors: zodErrors }
+      : { success: true }
+  );
 }
