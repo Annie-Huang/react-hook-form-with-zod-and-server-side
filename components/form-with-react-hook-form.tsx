f78dc0397e4f6c46d7601938d7a540d5c458e64c
Annie Huang <annie.hn@gmail.com>
2024-08-31 14:29:49 +1000

    Add validations into when register the field as well

--- components/form-with-react-hook-form.tsx
+++ components/form-with-react-hook-form.tsx
@@ -15,21 +15,29 @@ export const FormWithReactHookForm = () => {
   return (
     <form className='flex flex-col gap-y-2'>
       <input
-        {...register('email')}
+        {...register('email', { required: 'Email is required' })}
         type='email'
         required
         placeholder='Email'
         className='px-4 py-2 rounded'
       />
       <input
-        {...register('password')}
+        {...register('password', {
+          required: 'Password is required',
+          minLength: {
+            value: 10,
+            message: 'Password must be at least 10 characters',
+          },
+        })}
         type='password'
         required
         placeholder='Password'
         className='px-4 py-2 rounded'
       />
       <input
-        {...register('confirmPassword')}
+        {...register('confirmPassword', {
+          required: 'Confirm password is required',
+        })}
         type='password'
         required
         placeholder='Confirm password'
